eb29633b9f47b8e38fc91b03130af6bcf99a990d
mike <michaelrcampbell0916@gmail.com>
2024-02-16 19:34:12 -0700

    added asdf

--- pull.sh
+++ pull.sh
@@ -1,1 +1,2 @@
 cp -r /home/mike/.config/doom ./doom
+cp ~/.tool-versions ~/cb/asdf/.
--- push.sh
+++ push.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env sh
 rm -rf /home/mike/.config/doom
 cp -r ./doom /home/mike/.config/doom
+
+cp ~/cb/asdf/.tool-versions ~/.
